c3e3baac8b3debd8f4df5f4b81c3b643cbe05a45
ThomasB123 <thomas@thebutterfields.org>
2020-12-14 22:31:35 +0000

    day 10

--- 10.1.py
+++ 10.1.py
@@ -1,3 +1,16 @@
 infile = open('10input.txt','r')
+adapters = []
 for line in infile:
-    pass
+    adapters.append(int(line))
+adapters.sort()
+
+one = 0
+three = 1
+last = 0
+for x in range(len(adapters)):
+    if adapters[x] - last == 1:
+        one += 1
+    else:
+        three += 1
+    last = adapters[x]
+print(one*three)
--- 10.2.py
+++ 10.2.py
@@ -1,3 +1,32 @@
+import math
 infile = open('10input.txt','r')
+adapters = []
 for line in infile:
-    pass
+    adapters.append(int(line))
+adapters.sort()
+
+last = 0
+minus = {1:0,2:0,3:0,4:1}
+inaRow = 0
+count = 1
+multi = 0
+for x in range(len(adapters)):
+    if adapters[x] - last == 1:
+        inaRow += 1
+    else:
+        if inaRow > 1:
+            multi = 0
+            for i in range(0,inaRow):
+                multi += math.comb(inaRow-1,i)
+            multi -= minus[inaRow]
+            count *= multi
+        inaRow = 0
+    last = adapters[x]
+if inaRow > 1:
+    multi = 0
+    for i in range(0,inaRow):
+        multi += math.comb(inaRow-1,i)
+    multi -= minus[inaRow]
+    count *= multi
+
+print(count)
