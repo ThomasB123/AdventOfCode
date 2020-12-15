2e15a27974388c07c1ec9afad0e47aa323f322d4
ThomasB123 <44202676+ThomasB123@users.noreply.github.com>
2020-12-15 18:49:42 +0000

    Delete 01.py

--- 01.py
+++
@@ -1,8 +0,0 @@
-infile = open('01input.txt','r')
-match = {}
-for line in infile:
-    number = int(line[:-1])
-    if number in match:
-        print(number,number*(2020-number))
-        break
-    match[2020-number] = None
