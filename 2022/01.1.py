f043ca2b04415f2600a1c6f21dd5da94821373ab
Tom Butterfield <thomas.butterfield@sky.uk>
2022-12-01 09:43:17 +0000

    day 1

---
+++ 2022/01.1.py
@@ -0,0 +1,8 @@
+infile = open('01input.txt','r')
+calories = [0]
+for line in infile:
+    if len(line) > 1:
+        calories[-1] += int(line[:-1])
+    else:
+        calories.append(0)
+print(max(calories))
